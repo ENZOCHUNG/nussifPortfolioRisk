1665a4ed0d9ee31d9a469180634a38bbcd139045
ENZOCHUNG <chungyixuan0502@gmail.com>
2025-11-16 09:39:18 +0000

    lol

--- test.py
+++ test.py
@@ -1,19 +1,4 @@
-from ib_insync import IB
+import pandas as pd
 
-ib = IB()
-
-print("Connecting...")
-try:
-    ib.connect('127.0.0.1', 4002, clientId=999)
-    print("✓ Connected to IB Gateway!")
-
-    # Ping the API by requesting server time
-    server_time = ib.reqCurrentTime()
-    print("Server Time:", server_time)
-
-except Exception as e:
-    print("❌ Connection failed:", e)
-
-finally:
-    ib.disconnect()
-    print("Disconnected.")
+df = pd.read_parquet("weights.parquet")
+print(df)
